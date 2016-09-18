0fe16ea36b6dd0e262361dc8711009eaa60f62eb
Luia Yen <luia.yen@gmail.com>
2016-09-17 21:14:17 -0400

    added comments for clarity

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,8 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :year_published, numericality: { only_integer: true }
   
+  ## Scope is another word for SQL query; it is written within the model.
+  ## Syntax for scope changes frequently so don't need to memorize it, just look online.
   scope :alphabetical, -> { order('title') }
   scope :last_decade, -> { where('year_published > ?', 2006) }
 end
